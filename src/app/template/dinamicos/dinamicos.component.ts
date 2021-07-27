85bead20f913717bab004e95a900c999baf0527b
DarioOjeda <darioojeda_1995@hotmail.com>
2021-07-27 14:54:12 +0200

    lista de favoritos con ng for que genera inputs

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent{
 
-  @ViewChild('miFormulario') miFormulario!: NgForm;
-
-  constructor() { }
-
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre: 'Darío',
+    favoritos: [
+      { id: 1, nombre: 'Dark Souls 3'},
+      { id: 2, nombre: 'Hollow Knight'}
+    ]
   }
+
 
   guardar() {
     console.log('formulario posteado');
