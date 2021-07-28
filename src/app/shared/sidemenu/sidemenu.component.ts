ab36f7124262267b5701bbba817903a18b5978e5
DarioOjeda <darioojeda_1995@hotmail.com>
2021-07-28 10:27:00 +0200

    checkboxes en formularios por template

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -9,8 +9,11 @@ interface MenuItem {
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
-  styles: [
-  ]
+  styles: [`
+    li {
+      cursor:pointer; 
+    }
+  `]
 })
 export class SidemenuComponent{
 
@@ -25,7 +28,7 @@ export class SidemenuComponent{
     },
     {
       texto: 'Switches',
-      ruta: './template/SWITCHES'
+      ruta: './template/switches'
     }
   ]
 
