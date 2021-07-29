89b978cf5d4f3b5b3f19695c0ac6bb14592a946f
DarioOjeda <darioojeda_1995@hotmail.com>
2021-07-29 17:10:23 +0200

    validacion de ese username esta en uso

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -13,10 +13,24 @@ export class RegistroComponent implements OnInit {
   nombreApellidoPattern: string = '([a-zA-z]+) ([a-zA-Z]+)';
   emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
+  noPuedeSerCabesa( control: FormControl ) {
+    const valor: string = control.value?.trim().toLowerCase();
+
+    if( valor === 'cabesa') {
+      return {
+        noCabesa: true
+      }
+      // Esto ya se considera un error
+    }
+    return null;
+    // Si se retorna un null en una validación, eso quiere decir que no hubo ningún error
+  }
+
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPattern)] ],
     email: ['', [Validators.required, Validators.pattern(this.emailPattern) ] ],
     // Podriamos usar el Validators.email, pero F Herrera no se fía de esa validación.
+    username: ['',[Validators.required, this.noPuedeSerCabesa ], ],
   })
 
   constructor( private fb: FormBuilder ) { }
@@ -24,6 +38,8 @@ export class RegistroComponent implements OnInit {
   ngOnInit(): void {
     this.miFormulario.reset({
       nombre: 'Dario Ojeda',
+      email: 'dario_1887@gmail.com',
+      username: 'Cabesita'
     })
   }
 
