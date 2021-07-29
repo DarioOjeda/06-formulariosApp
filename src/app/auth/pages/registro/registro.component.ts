b9a078621f359869f7bd2e67691204cafa6e574c
DarioOjeda <darioojeda_1995@hotmail.com>
2021-07-29 16:56:45 +0200

    validación por email

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -11,14 +11,20 @@ export class RegistroComponent implements OnInit {
 
   // TODO: temporal
   nombreApellidoPattern: string = '([a-zA-z]+) ([a-zA-Z]+)';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPattern)] ],
+    email: ['', [Validators.required, Validators.pattern(this.emailPattern) ] ],
+    // Podriamos usar el Validators.email, pero F Herrera no se fía de esa validación.
   })
 
   constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Dario Ojeda',
+    })
   }
 
   campoNoValido( campo: string ) {
